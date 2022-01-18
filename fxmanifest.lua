41c6bb895ddb9a1eece3fa096eaa10c7f2a3b72b
marcostom32 <62820238+marcostom32@users.noreply.github.com>
2022-01-18 17:54:53 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,10 @@ version '1.0.0'
 
 shared_script 'config.lua'
 client_script 'client/main.lua'
-server_script 'server/main.lua'
+server_scripts  {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
 
 ui_page 'html/index.html'
 
